669273f0e96b62f827a703b3c0a091107c9071af
Lab User <labuser@coxlabphysiology1.rowland.org>
2011-06-19 15:15:03 -0400

    tested, memory expanded to 25G, need to sort out memory leak

--- caton/core.py
+++ caton/core.py
@@ -362,7 +362,7 @@ def extract_spikes(DatFileName,n_ch_dat,ChannelsToUse,ChannelGraph,max_spikes=No
         #fd.seek(0)
         FilteredChunk = filtfilt2d(b,a,DatChunk.astype(np.float64))    
         Threshold = THRESH_SD*np.median(np.abs(FilteredChunk),axis=0)/.6745    
-        logging.debug("Found threshold: %f" % Threshold)
+        logging.debug("Found threshold")
         
         spike_count = 0
         for s_start,s_end,keep_start,keep_end in chunk_bounds(n_samples,CHUNK_SIZE,CHUNK_OVERLAP):
@@ -370,12 +370,13 @@ def extract_spikes(DatFileName,n_ch_dat,ChannelsToUse,ChannelGraph,max_spikes=No
             # DatChunk = np.fromfile(fd,dtype=DTYPE,count=(s_end-s_start)*n_ch_dat).reshape(s_end-s_start,n_ch_dat)[:,ChannelsToUse]
             logging.debug("Reading chunk")
             DatChunk = read_data(DatFileName, s_start, s_end)[:,ChannelsToUse]
+            logging.debug("Chunk shape: %s" % str(DatChunk.shape))
             # fd.seek(fd.tell()-CHUNK_OVERLAP*n_ch_dat*np.nbytes[DTYPE])
             logging.debug("Filtering chunk")
             FilteredChunk = filtfilt2d(b,a,DatChunk.astype(np.float64))
             logging.debug("Thresholding chunk")
             BinaryChunk = (FilteredChunk < -Threshold).astype(np.int8) if DETECT_POSITIVE else np.abs(FilteredChunk < -Threshold).astype(np.int8)
-            logging.deubg("Finding connected components")
+            logging.debug("Finding connected components")
             IndListsChunk = connected_components(BinaryChunk,complete_if_none(ChannelGraph,N_CH),S_JOIN_CC)              
             logging.debug("Extracting spikes")
             for IndList in IndListsChunk:
@@ -384,6 +385,7 @@ def extract_spikes(DatFileName,n_ch_dat,ChannelsToUse,ChannelGraph,max_spikes=No
                     spike_count += 1
                     yield wave,s_start + s_peak,st
             ProgBar.update(spike_count,s_end)
+            logging.debug("Spike Count: %i" % spike_count)
             if max_spikes is not None and spike_count >= max_spikes: break
         ProgBar.finish()
          
